4de67b46addde408d7ef0f07b4ccc519b3d03d3a
Minerva <romanagrimm7@gmail.com>
2021-05-10 18:54:39 -0600

    Refactor app to be polymorph

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,8 +1,10 @@
 class Shout < ApplicationRecord
 	
   belongs_to :user
+  belongs_to :content, polymorphic: true
 
-  validates :body, presence: true, Length: { in: 1..144}
+  
+
   validates :user, presence: true
 
   default_scope { order(created_at: :desc)}
